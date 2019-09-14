ba96bc80b8570077ddd35376cedfe5208240df9b
ryo <ba068082@gmail.com>
2019-09-14 17:59:25 +0900

    wantedlyページのレイアウトを変更

--- src/views/Wantedly.vue
+++ src/views/Wantedly.vue
@@ -4,22 +4,20 @@
       <v-flex xs10 offset-xs1>
     <div v-for="(card, index) in wantedly" :key="index">
       <v-card class="mb-5">
-        <v-img
-            src="https://picsum.photos/400/800"
-            aspect-ratio="2.75"
-            style="height: 50px"
-        ></v-img>
-
         <v-card-title>
-          <div>
-            <h1 class="headline mt-0 text-xs-left">{{ card.title }}</h1>
-            <p class="body-2 text-xs-left">{{ card.name }}</p>
-            <p class="body-2 text-xs-left"> {{ card.message }} </p>
-          </div>
+          <v-layout>
+            <v-flex xs3>
+              <v-img :src="`https://cdn.vuetifyjs.com/images/lists/${index + 1}.jpg`"></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <h1 class="headline mt-0 text-xs-left">{{ card.title }}</h1>
+              <p class="body-2 text-xs-left">{{ card.name }}</p>
+              <p class="body-2 text-xs-left"> {{ card.message }} </p>
+            </v-flex>
+          </v-layout>
         </v-card-title>
-
         <v-card-actions>
-          <v-btn color="primary" style="width: 100%" to="/task">Start</v-btn>
+          <v-btn color="primary" style="width: 100%" to="/task">start</v-btn>
         </v-card-actions>
       </v-card>
     </div>
