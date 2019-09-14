633606aaa095790e9f4afc3287ec6b46263f43e4
Satoshi Ebisawa <e.satoshi@gmail.com>
2019-09-14 17:27:27 +0900

    enable to post from

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Top from '@/views/Top'
 import Signin from '@/components/Signin'
 import Signout from '@/views/Signout'
 import Wantedly from '@/views/Wantedly'
+import Task from '@/components/Task'
 
 import firebase from 'firebase'
 
@@ -14,33 +15,38 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
-            path: '/',
-            name: 'index',
-            component: Top
-        },
-        {
-            path: '/Signin',
-            name: 'signin',
-            component: Signin
-        },
-        {
-            path: '/Signout',
-            name: 'signout',
-            component: Signout,
-            meta: { requiresAuth: true }
-        },
-        {
-          path: '/wantedly',
-          name: 'wantedly',
-          component: Wantedly,
-        },
+        path: '/',
+        name: 'index',
+        component: Top
+    },
+    {
+        path: '/Signin',
+        name: 'signin',
+        component: Signin
+    },
+    {
+        path: '/Signout',
+        name: 'signout',
+        component: Signout,
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/wantedly',
+        name: 'wantedly',
+        component: Wantedly,
+    },
+    {
+        path: '/task',
+        name: 'task',
+        component: Task,
+    },
     ]
 })
 
 router.beforeEach((to, from, next) => {
     const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
     if (requiresAuth) {
-        firebase.auth().onAuthStateChanged(function(user) {
+        firebase.auth().onAuthStateChanged(function (user) {
             if (user) {
                 next()
             } else {
